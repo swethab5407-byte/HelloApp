7ad1b155bb7df91710232014125b678ef6088096
Swetha <swethab5407@gmail.com>
2026-03-26 14:14:13 +0530

    UC7

---
+++ HellloAppp.java
@@ -0,0 +1,24 @@
+// UC6 - Using substring to remove trailing comma
+
+public class HelloApp {
+
+    public static void main(String[] args) {
+
+        if (args.length > 0) {
+
+            String names = "";
+
+            for (String name : args) {
+                names = names + name + ", ";
+            }
+
+            // Remove last ", "
+            names = names.substring(0, names.length() - 2);
+
+            System.out.println("Hello " + names);
+
+        } else {
+            System.out.println("Hello World");
+        }
+    }
+}
